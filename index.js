3cc60ddab2f311e93160846e26b1bf793742a1ad
Yalwin <alwin.velasco@unet.edu.ve>
2021-03-10 19:28:07 -0430

    ....

--- index.js
+++ index.js
@@ -3,15 +3,27 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
+
   socket.on('chat message', msg => {
     io.emit('chat message', msg);
   });
+
 });
+
+io.sockets.on('connection', function(socket){
+
+   socket.on('user image',function(image){
+       io.sockets.emit('addimage',' ', image);
+       console.log('epale menor');
+   });
+});
+
 
 http.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);
